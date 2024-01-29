7f1a829332ada5db9ec885e4ad43a6b81caeffca
mintcd <dangminh2208@gmail.com>
2024-01-29 08:44:45 +0700

    Test publishing

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from adafruit_mqtt import Adafruit_MQTT
+import time
+import random
 
 instance = Adafruit_MQTT()
-instance.connect()
+
+counter = 10
+
+while True: 
+  time.sleep(1)
+  counter = counter - 1
+  if counter <= 0:
+    counter = 10
+    temp = random.randint(10,20)
+    instance.client.publish("sensor1", temp)
+    hum = random.randint(10,20)
+    instance.client.publish("sensor3", hum)
+  pass
